c01898fa2fc617803487b44f193fbb1b55b13b81
Marc Alan Phillips <mphillips@instructure.com>
2016-10-17 18:51:11 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import BoxPortal from '../lib/components/BoxPortal'
+
+ReactDOM.render(
+  <div>
+    <BoxPortal />
+  </div>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    historyApiFallback: true
+  },
+  plugins: [
+  ]
+}
